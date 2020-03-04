cd6b23388635ba44c6bf4fe76740e2ef44771326
ionutflorina <61784629+ionutflorina@users.noreply.github.com>
2020-03-04 15:19:51 +0200

    Initial commit

---
+++ CursGarmin/app/src/main/java/com/garmin/android/cursgarmin/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.garmin.android.cursgarmin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
